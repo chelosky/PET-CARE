18d5a339c9d522104c8f57010af83b164afdb2e1
Chelosky <marcelolazo19972013@hotmail.com>
2020-03-17 18:31:41 -0300

    Initial Views of petcare

---
+++ src/main/java/com/fillikenesucn/petcare/activity/HomeFragmentActivity.java
@@ -0,0 +1,32 @@
+package com.fillikenesucn.petcare.activity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.fillikenesucn.petcare.R;
+
+public class HomeFragmentActivity extends FragmentActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+    protected ActionBar mActionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_fragment);
+        mActionBar = getActionBar();
+        mActionBar.hide();
+        mActionBar.setDisplayShowTitleEnabled(false);
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayHomeAsUpEnabled(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(HomeFragmentActivity.this, MainMenuFragmentActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
