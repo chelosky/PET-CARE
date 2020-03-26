9b2d8b031c64c9d86b9f86604a8dc2cc30e563ff
Rodrigo <rdoratm@outlook.com>
2020-03-25 23:58:03 -0300

    Initial json serializable data

--- src/main/java/com/fillikenesucn/petcare/activity/PETCARE/RegisterPetFragmentActivity.java
+++ src/main/java/com/fillikenesucn/petcare/activity/PETCARE/RegisterPetFragmentActivity.java
@@ -1,16 +1,30 @@
 package com.fillikenesucn.petcare.activity.PETCARE;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+// Testing
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
 
 import com.fillikenesucn.petcare.R;
+import com.fillikenesucn.petcare.activity.PETCARE.models.Pet;
+import com.fillikenesucn.petcare.activity.PETCARE.utils.IOHelper;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -23,13 +37,24 @@ public class RegisterPetFragmentActivity extends FragmentActivity {
     private int nYearIni, nMonthIni, nDayIni, sYearIni, sMonthIni, sDayIni;
     private Calendar calendar = Calendar.getInstance();
     private Spinner spinner;
+
+    // Testing
+    private Button btnRegist;
+    private EditText txtName;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private EditText txtAddress;
+    private EditText txtAllergies;
+    private TextView txtTest;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_pet_fragment);
         this.spinner = findViewById(R.id.spinnerRegisterPet);
-        List<String> pets = new ArrayList<>();
-        pets.add(0, "Seleccione Tipo");
+        final List<String> pets = new ArrayList<>();
+        pets.add(0, "Seleccione Especie");
         pets.add("Perro");
         pets.add("Gato");
 
@@ -43,6 +68,12 @@ public class RegisterPetFragmentActivity extends FragmentActivity {
 
         et_fechaNacimiento = (EditText)findViewById(R.id.fechaNacimiento);
         btnFechaNacimiento = (Button)findViewById(R.id.btnFechaNacimiento);
+        txtTest = (TextView) findViewById(R.id.txtTest);
+        txtName = (EditText) findViewById(R.id.txtName);
+        radioGroup = (RadioGroup) findViewById(R.id.radio);
+        spinner = findViewById(R.id.spinnerRegisterPet);
+        txtAddress = (EditText) findViewById(R.id.txtAddress);
+        txtAllergies = (EditText) findViewById(R.id.txtAllergies);
 
         btnFechaNacimiento.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +88,49 @@ public class RegisterPetFragmentActivity extends FragmentActivity {
                 dpd.show();
             }
         });
+
+        // Testing
+        Button tempLeer = (Button)findViewById(R.id.leerjson);
+        tempLeer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReadJsonFile();
+                Toast.makeText(RegisterPetFragmentActivity.this, "LISTO2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnRegist = (Button) findViewById(R.id.btnRegist);
+        btnRegist.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // get selected radio button from radioGroup
+                        int selectedId = radioGroup.getCheckedRadioButtonId();
+                        // find the radiobutton by returned id
+                        radioButton = (RadioButton) findViewById(selectedId);
+
+                        String name = txtName.getText().toString();
+                        String sex = radioButton.getText().toString();
+                        String species = spinner.getSelectedItem().toString();
+                        String birthdate = et_fechaNacimiento.getText().toString();
+                        String address = txtAddress.getText().toString();
+                        String allergies = txtAllergies.getText().toString();
+
+                        Pet pet = new Pet(name,sex,birthdate,address,allergies,species);
+                        Gson gson = new Gson();
+                        WriteJsonFile(gson.toJson(pet));
+
+                        Toast.makeText(RegisterPetFragmentActivity.this, "LISTO1", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    private void ReadJsonFile(){
+        txtTest.setText(IOHelper.ReadFileString(this));
+    }
+
+    private void WriteJsonFile(String stringvalue){
+        IOHelper.WriteJson(this,"petcare.txt",stringvalue);
     }
 
 }
---
+++ src/main/java/com/fillikenesucn/petcare/activity/PETCARE/models/Acontecimiento.java
@@ -0,0 +1,37 @@
+package com.fillikenesucn.petcare.activity.PETCARE.models;
+
+public class Acontecimiento {
+    private String titulo;
+    private String fecha;
+    private String descripcion;
+
+    public Acontecimiento(String titulo, String fecha, String descripcion) {
+        this.titulo = titulo;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
